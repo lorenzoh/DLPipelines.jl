a2ca2a382ea398c90ef49057ff6405721e236198
lorenzoh <lorenz.ohly@gmail.com>
2021-03-25 11:31:44 +0100

    remove test deps

--- test/runtests.jl
+++
@@ -1,8 +0,0 @@
-using DLPipelines
-using DataAugmentation
-using DLPipelines: Training, Validation, Inference
-using DLPipelines: apply, apply!, invert, invert!
-using Colors: RGB
-using Test
-using TestSetExtensions
-using StaticArrays
